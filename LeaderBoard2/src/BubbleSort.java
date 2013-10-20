f63b078d4e61e58b60098a094ab798c7fc6a8490
Steve <Steve@Steve-DellPC>
2013-10-20 08:51:40 -0700

    first commit (ignore leaderboard)

---
+++ LeaderBoard2/src/BubbleSort.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BubbleSort {
+	public List<KeyValuePairs> sort(ArrayList<KeyValuePairs> unsorted){
+		for (int i = 0; i < (unsorted.size() -1); i++){
+			if (true){
+				
+			}
+		}
+		
+		
+		return null;
+		
+	
+	//public int translate (KeyValuePairs x){
+		//String p = x.getValue();
+		//int q = Integer.parseInt(p);
+		//return q;
+	//}
+}
+}
